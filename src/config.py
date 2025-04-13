21c01b55dcc590cce882c657abd1cda85df12326
Sam <sdlagrosas.work@gmail.com>
2025-04-13 23:41:20 +0800

    increase block max fall speed

--- src/config.py
+++ src/config.py
@@ -3,7 +3,7 @@ import pygame
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 BLOCK_START_SPEED = 1.0
-BLOCK_MAX_SPEED = 0.3
+BLOCK_MAX_SPEED = 0.08
 
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 GAME_CLOCK = pygame.time.Clock()

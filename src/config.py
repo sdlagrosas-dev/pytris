b844d43997fe785b15563c43aa0b5ff1e58b54dd
Sam <sdlagrosas.work@gmail.com>
2025-04-15 22:24:59 +0800

    change default bg

--- src/config.py
+++ src/config.py
@@ -7,7 +7,7 @@ BLOCK_MAX_SPEED = 0.08
 
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 GAME_CLOCK = pygame.time.Clock()
-BG_MENU = pygame.image.load("assets/Background.png")
+BG_MENU = pygame.image.load("assets/PygTris_Background_2.jpg")
 
 
 def get_font(size):
